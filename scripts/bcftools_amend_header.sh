803e3e9762375d864b7baff3caba62f2f7e9e8f4
Miguel Brown <miguel.a.brown@gmail.com>
2024-10-18 16:19:46 -0400

    :wrench: revert to bash entry, correct missing ; add missing tabix

--- scripts/bcftools_amend_header.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# script to use bcftools to amend the header in ploidy-adjusted file
-set -xeo pipefail
-
-input_vcf=$1
-mod_vcf=$2
-awk 'NR == FNR && $line ~ /^##GATKCommandLine.HaplotypeCaller/ {sub(/Caller,/, "Caller_rpt_subset,") newcmd=$line}; NR != FNR; NR != FNR && $line ~ /^##GATKCommandLine.HaplotypeCaller/ {print newcmd}' <(bcftools head $mod_vcf) <(bcftools head $input_vcf) > header_build.txt
